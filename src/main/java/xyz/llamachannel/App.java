d5b83edf31bbc929f05dffa7bc4082a47ec59b2f
llamachannel <llamachannel@fastmail.com>
2021-05-10 19:28:31 -0400

    Initial commit

---
+++ src/main/java/xyz/llamachannel/App.java
@@ -0,0 +1,11 @@
+package xyz.llamachannel;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
